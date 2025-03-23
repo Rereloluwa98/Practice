ea090b6b6803d835eed684add2a74e92e20e5a31
Emmanuel Rereloluwa Oluwasijibomi <77580461+Rereloluwa98@users.noreply.github.com>
2025-03-23 17:24:12 +0000

    grades_if_else_elif.py
    If else elif statements praactice

---
+++ grades_if_else_elif.py
@@ -0,0 +1,9 @@
+grade = int(input("Enter you grade:"))
+if grade > 85:
+  print("Wow, top 10%! you get and A+")
+elif grade > 75:
+  print("Excellent! you get an A")
+elif grade >= 60:
+  print("Yay! you passed.")
+else:
+  print("Nay! you failed.")
